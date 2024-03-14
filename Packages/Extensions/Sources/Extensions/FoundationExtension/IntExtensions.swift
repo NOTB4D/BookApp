d6dc5c6a04f91fafe9f81c8f6c725b71abd87a72
Eser Kucuker <eser.kucuker@mobven.com>
2024-03-14 10:29:21 +0300

    add Int? extension

---
+++ Packages/Extensions/Sources/Extensions/FoundationExtension/IntExtensions.swift
@@ -0,0 +1,14 @@
+//
+//  IntExtensions.swift
+//
+//
+//  Created by Eser Kucuker on 14.03.2024.
+//
+
+import Foundation
+
+public extension Int? {
+    var intValue: Int {
+        self ?? 0
+    }
+}
